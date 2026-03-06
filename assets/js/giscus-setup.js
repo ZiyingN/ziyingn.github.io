651058362e57a32e9f2f4d873c19f979535256ba
ZiyingN <88420344+ZiyingN@users.noreply.github.com>
2026-03-06 19:15:59 +0000

    Deploying to gh-pages from @ ZiyingN/ziyingn.github.io@618ed1fab65d54a900452b89808d3795c8286f40 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "ZiyingN/Z.Ni.github.io",
+    "data-repo": "ZiyingN/ziyingn.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
