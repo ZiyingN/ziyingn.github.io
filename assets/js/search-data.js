4005a2fa5e9c0e932487a7554d68b81c04031e35
ZiyingN <88420344+ZiyingN@users.noreply.github.com>
2026-03-06 19:36:41 +0000

    Deploying to gh-pages from @ ZiyingN/ziyingn.github.io@991ce56858ffafac4fc6e01052d88a46a7abaf97 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -405,14 +405,14 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-one-paper-has-been-accepted-by-sigir2025",
-          title: 'One Paper has been accepted by SIGIR2025',
-          description: "",
-          section: "News",},{id: "news-one-paper-has-been-accepted-by-icml2025",
+            },},{id: "news-one-paper-has-been-accepted-by-icml2025",
           title: 'One Paper has been accepted by ICML2025',
           description: "",
-          section: "News",},{id: "news-one-paper-has-been-accepted-by-tmm",
-          title: 'One Paper has been accepted by TMM',
+          section: "News",},{id: "news-one-paper-has-been-accepted-by-tc",
+          title: 'One Paper has been accepted by TC',
+          description: "",
+          section: "News",},{id: "news-one-paper-has-been-accepted-by-dac2026",
+          title: 'One Paper has been accepted by DAC2026',
           description: "",
           section: "News",},{
         id: 'social-email',
